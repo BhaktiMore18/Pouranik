eb7a3c151a77a88d53c5bd045222003013ee314b
Anshika Tiwari <tiwarianshika728@gmail.com>
2025-08-09 16:55:41 +0530

    add ESLINT 8 config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  ignorePatterns: ["dist"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
+    // Note: eslint-plugin-react-refresh doesn't have an ESLint 8-ready "extends"
+    // You may need to manually add its rules if needed
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["react", "react-hooks", "react-refresh"],
+  rules: {
+    "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+  },
+};
