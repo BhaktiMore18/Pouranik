709666471ee43c273967ccbf51c82fa32f4f3db1
Vidhi-Ajmera <vidhi2005ajmera@gmail.com>
2025-09-15 12:11:27 +0530

    privacy policy page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,17 @@
-import BookDetail from '../pages/BookDetail';
-import Genres from '../pages/Genres';
+import BookDetail from "../pages/BookDetail";
+import Genres from "../pages/Genres";
 import { Routes, Route } from "react-router-dom";
-import Home from '../pages/Home';
-import Explore from '../pages/Explore';
-import AboutUs from '../pages/about';
-import Library from '../pages/Library';
-import SignIn from '../pages/SignIn';
-import Reviews from '../pages/Reviews';
-import Community from '../pages/Community';
-import ClubPage from '../pages/ClubPage';
-import TimerPage from '../pages/TimerPage';
-import AnalyticsPage from '../pages/AnalyticsPage';
+import Home from "../pages/Home";
+import Explore from "../pages/Explore";
+import AboutUs from "../pages/about";
+import Library from "../pages/Library";
+import SignIn from "../pages/SignIn";
+import Reviews from "../pages/Reviews";
+import Community from "../pages/Community";
+import ClubPage from "../pages/ClubPage";
+import TimerPage from "../pages/TimerPage";
+import AnalyticsPage from "../pages/AnalyticsPage";
+import Privacy from "../pages/privacy.jsx";
 
 export default function AppRoutes() {
   return (
@@ -20,13 +21,14 @@ export default function AppRoutes() {
       <Route path="/genres" element={<Genres />} />
       <Route path="/explore" element={<Explore />} />
       <Route path="/about" element={<AboutUs />} />
-      <Route path='/library' element={<Library />} />
-      <Route path='/timerpage' element={<TimerPage/>} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/timerpage" element={<TimerPage />} />
       <Route path="/analytics" element={<AnalyticsPage />} />
-      <Route path='/signup' element={<SignIn />} />
-      <Route path='/book/:id/reviews' element={<Reviews />} />
-      <Route path='/community' element={<Community />} />
-      <Route path='/club' element={<ClubPage />} />
+      <Route path="/signup" element={<SignIn />} />
+      <Route path="/book/:id/reviews" element={<Reviews />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/club" element={<ClubPage />} />
+      <Route path="/privacy" element={<Privacy />} />
     </Routes>
   );
 }
